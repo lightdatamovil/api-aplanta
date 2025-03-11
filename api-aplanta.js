73c51e2a8e3dc59d6309dc29ec55037fd9329bb9
LightData <lightdatamovil@gmail.com>
2025-03-11 14:08:07 -0300

    Add files via upload

---
+++ api-aplanta.js
@@ -0,0 +1,20 @@
+import express, { json, urlencoded } from 'express';
+import a_planta from './route/aplanta.js';
+import { redisClient } from './db.js';
+
+
+const app = express();
+
+app.use(json({ limit: '50mb' }));
+app.use(urlencoded({ limit: '50mb', extended: true }));
+app.use(json());
+
+const PORT = process.env.PORT || 13000;
+
+app.use("/api", a_planta)
+
+await redisClient.connect();
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
---
+++ route/aplanta.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { getCompanyById } from '../db.js';
+import { aplanta } from '../controller/aplantaController.js';
+
+const a_planta = Router();
+
+a_planta.post('/aplanta', async (req, res) => {
+   // const errorMessage = verifyParamaters(req.body, ['dataQr', 'deviceFrom']);
+
+   /// if (errorMessage) {
+      //  return res.status(400).json({ message: errorMessage });
+    //}
+
+    const { companyId, userId, profile, dataQr, autoAssign } = req.body;
+
+    try {
+        const company = await getCompanyById(companyId);
+
+        const result = await aplanta (company, JSON.parse(dataQr), userId, profile, autoAssign);
+
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+export default a_planta;
