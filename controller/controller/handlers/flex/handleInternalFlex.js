a7ba05106dbf061acf722b28ac4a69e33761078d
Agustt123 <agustintracheskyoficial@gmail.com>
2025-05-30 10:53:13 -0300

    cambiazos epicos

--- controller/controller/handlers/flex/handleInternalFlex.js
+++ controller/controller/handlers/flex/handleInternalFlex.js
@@ -11,55 +11,91 @@ import { logCyan } from "../../../../src/funciones/logsCustom.js";
 /// Checkeo si el envío ya fue colectado cancelado o entregado
 /// Actualizo el estado del envío y lo envío al microservicio de estados
 /// Asigno el envío al usuario si es necesario
-export async function handleInternalFlex(dbConnection, companyId, userId, dataQr, account) {
-    const senderId = dataQr.sender_id;
-    const mlShipmentId = dataQr.id;
+export async function handleInternalFlex(
+  dbConnection,
+  companyId,
+  userId,
+  dataQr,
+  account
+) {
+  const senderId = dataQr.sender_id;
+  const mlShipmentId = dataQr.id;
 
-    let shipmentId;
+  let shipmentId;
 
-    /// Busco el envio
-    const sql = `
+  /// Busco el envio
+  const sql = `
         SELECT did
         FROM envios 
         WHERE ml_shipment_id = ? AND ml_vendedor_id = ? 
         LIMIT 1
     `;
 
-    let resultBuscarEnvio = await executeQuery(dbConnection, sql, [mlShipmentId, senderId]);
+  let resultBuscarEnvio = await executeQuery(dbConnection, sql, [
+    mlShipmentId,
+    senderId,
+  ]);
 
-    /// Si no existe, lo inserto y tomo el did
-    if (resultBuscarEnvio.length === 0) {
-        shipmentId = await insertEnvios(dbConnection, companyId, account.didCliente, account.didCuenta, dataQr, 1, 0);
-        resultBuscarEnvio = await executeQuery(dbConnection, sql, [mlShipmentId, senderId]);
-        logCyan("Inserte el envio");
-    } else {
-        logCyan("Encontre el envio");
-    }
+  /// Si no existe, lo inserto y tomo el did
+  if (resultBuscarEnvio.length === 0) {
+    shipmentId = await insertEnvios(
+      dbConnection,
+      companyId,
+      account.didCliente,
+      account.didCuenta,
+      dataQr,
+      1,
+      0,
+      0
+    );
+    resultBuscarEnvio = await executeQuery(dbConnection, sql, [
+      mlShipmentId,
+      senderId,
+    ]);
+    logCyan("Inserte el envio");
+  } else {
+    logCyan("Encontre el envio");
+  }
 
-    const row = resultBuscarEnvio[0];
+  const row = resultBuscarEnvio[0];
 
-    shipmentId = row.did;
+  shipmentId = row.did;
 
-    /// Checkeo si el envío ya fue colectado cancelado o entregado
-    const check = await checkearEstadoEnvio(dbConnection, shipmentId);
-    if (check) return check;
-    logCyan("El envio no fue colectado cancelado o entregado");
+  /// Checkeo si el envío ya fue colectado cancelado o entregado
+  const check = await checkearEstadoEnvio(dbConnection, shipmentId);
+  if (check) return check;
+  logCyan("El envio no fue colectado cancelado o entregado");
 
-    const queryUpdateEnvios = `
+  const queryUpdateEnvios = `
                 UPDATE envios 
                 SET ml_qr_seguridad = ?
                 WHERE superado = 0 AND elim = 0 AND did = ?
                 LIMIT 1
             `;
 
-    await executeQuery(dbConnection, queryUpdateEnvios, [JSON.stringify(dataQr), shipmentId]);
-    logCyan("Actualice el ml_qr_seguridad del envio");
+  await executeQuery(dbConnection, queryUpdateEnvios, [
+    JSON.stringify(dataQr),
+    shipmentId,
+  ]);
+  logCyan("Actualice el ml_qr_seguridad del envio");
 
-    /// Actualizo el estado del envío y lo envío al microservicio de estados
-    await sendToShipmentStateMicroService(companyId, userId, shipmentId);
-    logCyan("Actualice el estado del envio y lo envie al microservicio de estados");
+  /// Actualizo el estado del envío y lo envío al microservicio de estados
 
-    const body = await informe(dbConnection, companyId, account.didCliente, userId, shipmentId);
-    return { success: true, message: "Paquete insertado y puesto a planta  - FLEX", body: body };
+  await sendToShipmentStateMicroService(companyId, userId, shipmentId);
+  logCyan(
+    "Actualice el estado del envio y lo envie al microservicio de estados"
+  );
 
+  const body = await informe(
+    dbConnection,
+    companyId,
+    account.didCliente,
+    userId,
+    shipmentId
+  );
+  return {
+    success: true,
+    message: "Paquete insertado y puesto a planta  - FLEX",
+    body: body,
+  };
 }
