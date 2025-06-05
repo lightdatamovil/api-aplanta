8742596d8ae18901cd8afb8437c3763b2b17fb22
Agustt123 <agustintracheskyoficial@gmail.com>
2025-06-05 16:05:04 -0300

    ca

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-import redis from 'redis';
-import dotenv from 'dotenv';
-import mysql from 'mysql';
-import { logRed, logYellow } from './src/funciones/logsCustom.js';
+import redis from "redis";
+import dotenv from "dotenv";
+import mysql from "mysql";
+import { logRed, logYellow } from "./src/funciones/logsCustom.js";
 
 dotenv.config({ path: process.env.ENV_FILE || ".env" });
 
@@ -10,15 +10,15 @@ const redisPort = process.env.REDIS_PORT;
 const redisPassword = process.env.REDIS_PASSWORD;
 
 export const redisClient = redis.createClient({
-    socket: {
-        host: redisHost,
-        port: redisPort,
-    },
-    password: redisPassword,
+  socket: {
+    host: redisHost,
+    port: redisPort,
+  },
+  password: redisPassword,
 });
 
-redisClient.on('error', (err) => {
-    logRed(`Error al conectar con Redis: ${error.stack}`);
+redisClient.on("error", (err) => {
+  logRed(`Error al conectar con Redis: ${error.stack}`);
 });
 
 let companiesList = {};
@@ -27,195 +27,205 @@ let accountList = {};
 let driverList = {};
 
 export function getProdDbConfig(company) {
-    return {
-        host: "bhsmysql1.lightdata.com.ar",
-        user: company.dbuser,
-        password: company.dbpass,
-        database: company.dbname
-    };
+  return {
+    host: "bhsmysql1.lightdata.com.ar",
+    user: company.dbuser,
+    password: company.dbpass,
+    database: company.dbname,
+  };
 }
 export function getLocalDbConfig() {
-    return {
-      //  host: "localhost",
-        host: "149.56.182.49",
-        user:"ulogs" ,
-        password: "aplanta2025*",
-        database: "data",
-       port: 44345
-    };
+  return {
+    //  host: "localhost",
+    host: "149.56.182.49",
+    user: "ulogs",
+    password: "aplanta2025*",
+    database: "data",
+    port: 44345,
+  };
 }
 
 async function loadCompaniesFromRedis() {
-    try {
-        const companiesListString = await redisClient.get('empresasData');
+  try {
+    const companiesListString = await redisClient.get("empresasData");
 
-        companiesList = JSON.parse(companiesListString);
-
-    } catch (error) {
-        logRed(`Error en loadCompaniesFromRedis: ${error.stack}`);
-        throw error;
-    }
+    companiesList = JSON.parse(companiesListString);
+  } catch (error) {
+    logRed(`Error en loadCompaniesFromRedis: ${error.stack}`);
+    throw error;
+  }
 }
 
 export async function getCompanyById(companyId) {
-    try {
-        let company = companiesList[companyId];
+  try {
+    let company = companiesList[companyId];
 
-        if (company == undefined || Object.keys(companiesList).length === 0) {
-            try {
-                await loadCompaniesFromRedis();
+    if (company == undefined || Object.keys(companiesList).length === 0) {
+      try {
+        await loadCompaniesFromRedis();
 
-                company = companiesList[companyId];
-            } catch (error) {
-                logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
-                throw error;
-            }
-        }
-
-        return company;
-    } catch (error) {
-        logRed(`Error en getCompanyById: ${error.stack}`);
+        company = companiesList[companyId];
+      } catch (error) {
+        logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
         throw error;
+      }
     }
+
+    return company;
+  } catch (error) {
+    logRed(`Error en getCompanyById: ${error.stack}`);
+    throw error;
+  }
 }
 
 export async function getCompanyByCode(companyCode) {
-    try {
-        let company;
+  try {
+    let company;
 
-        if (Object.keys(companiesList).length === 0) {
-            try {
-                await loadCompaniesFromRedis();
-            } catch (error) {
-                logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
-                throw error;
-            }
-        }
-
-        for (const key in companiesList) {
-            if (companiesList.hasOwnProperty(key)) {
-                const currentCompany = companiesList[key];
-                if (String(currentCompany.codigo) === String(companyCode)) {
-                    company = currentCompany;
-                    break;
-                }
-            }
-        }
-
-        return company;
-    } catch (error) {
-        logRed(`Error en getCompanyByCode: ${error.stack}`);
+    if (Object.keys(companiesList).length === 0) {
+      try {
+        await loadCompaniesFromRedis();
+      } catch (error) {
+        logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
         throw error;
+      }
     }
+    console.log(`Buscando compañía con código: ${companyCode}`);
+    console.log(
+      `Lista de compañías cargadas: ${JSON.stringify(companiesList)}`
+    );
+
+    for (const key in companiesList) {
+      if (companiesList.hasOwnProperty(key)) {
+        const currentCompany = companiesList[key];
+        if (String(currentCompany.codigo) === String(companyCode)) {
+          company = currentCompany;
+          break;
+        }
+      }
+    }
+
+    return company;
+  } catch (error) {
+    logRed(`Error en getCompanyByCode: ${error.stack}`);
+    throw error;
+  }
 }
 
 async function loadAccountList(dbConnection, companyId, senderId) {
-    try {
-        const querySelectClientesCuentas = `
+  try {
+    const querySelectClientesCuentas = `
             SELECT did, didCliente, ML_id_vendedor 
             FROM clientes_cuentas 
             WHERE superado = 0 AND elim = 0 AND tipoCuenta = 1 AND ML_id_vendedor != ''
         `;
 
-        const result = await executeQuery(dbConnection, querySelectClientesCuentas);
+    const result = await executeQuery(dbConnection, querySelectClientesCuentas);
 
-        if (!accountList[companyId]) {
-            accountList[companyId] = {};
-        }
-
-        result.forEach(row => {
-            const keySender = row.ML_id_vendedor;
-
-            if (!accountList[companyId][keySender]) {
-                accountList[companyId][keySender] = {};
-            }
-
-            accountList[companyId][keySender] = {
-                didCliente: row.didCliente,
-                didCuenta: row.did,
-            };
-        });
-
-        return accountList[companyId] ? accountList[companyId][senderId] : null;
-    } catch (error) {
-        logRed(`Error en obtenerMisCuentas: ${error.stack}`);
-        throw error;
+    if (!accountList[companyId]) {
+      accountList[companyId] = {};
     }
+
+    result.forEach((row) => {
+      const keySender = row.ML_id_vendedor;
+
+      if (!accountList[companyId][keySender]) {
+        accountList[companyId][keySender] = {};
+      }
+
+      accountList[companyId][keySender] = {
+        didCliente: row.didCliente,
+        didCuenta: row.did,
+      };
+    });
+
+    return accountList[companyId] ? accountList[companyId][senderId] : null;
+  } catch (error) {
+    logRed(`Error en obtenerMisCuentas: ${error.stack}`);
+    throw error;
+  }
 }
 
 export async function getAccountBySenderId(dbConnection, companyId, senderId) {
-    try {
-        if (accountList === undefined || accountList === null || Object.keys(accountList).length === 0 || !accountList[companyId]) {
-            await loadAccountList(dbConnection, companyId, senderId);
-        }
-
-        const account = accountList[companyId][senderId];
-
-        return account;
-    } catch (error) {
-        logRed(`Error en getAccountBySenderId: ${error.stack}`);
-        throw error;
+  try {
+    if (
+      accountList === undefined ||
+      accountList === null ||
+      Object.keys(accountList).length === 0 ||
+      !accountList[companyId]
+    ) {
+      await loadAccountList(dbConnection, companyId, senderId);
     }
+
+    const account = accountList[companyId][senderId];
+
+    return account;
+  } catch (error) {
+    logRed(`Error en getAccountBySenderId: ${error.stack}`);
+    throw error;
+  }
 }
 
 async function loadClients(dbConnection, companyId) {
-    if (!clientList[companyId]) {
-        clientList[companyId] = {}
-    }
+  if (!clientList[companyId]) {
+    clientList[companyId] = {};
+  }
 
-    try {
-        const queryUsers = "SELECT * FROM clientes";
-        const resultQueryUsers = await executeQuery(dbConnection, queryUsers, []);
+  try {
+    const queryUsers = "SELECT * FROM clientes";
+    const resultQueryUsers = await executeQuery(dbConnection, queryUsers, []);
 
-        resultQueryUsers.forEach(row => {
-            const client = row.did;
+    resultQueryUsers.forEach((row) => {
+      const client = row.did;
 
-            if (!clientList[companyId][client]) {
-                clientList[companyId][client] = {};
-            }
+      if (!clientList[companyId][client]) {
+        clientList[companyId][client] = {};
+      }
 
-            clientList[companyId][client] = {
-                fecha_sincronizacion: row.fecha_sincronizacion,
-                did: row.did,
-                codigo: row.codigoVinculacionLogE,
-                nombre: row.nombre_fantasia,
-            };
-        });
-    } catch (error) {
-        logRed(`Error en loadClients para la compañía ${companyId}: ${error.stack}`);
-        throw error;
-    }
+      clientList[companyId][client] = {
+        fecha_sincronizacion: row.fecha_sincronizacion,
+        did: row.did,
+        codigo: row.codigoVinculacionLogE,
+        nombre: row.nombre_fantasia,
+      };
+    });
+  } catch (error) {
+    logRed(
+      `Error en loadClients para la compañía ${companyId}: ${error.stack}`
+    );
+    throw error;
+  }
 }
 
 export async function getClientsByCompany(dbConnection, companyId) {
-    try {
-        let companyClients = clientList[companyId];
+  try {
+    let companyClients = clientList[companyId];
 
-        if (companyClients == undefined || Object.keys(clientList).length === 0) {
-            try {
-                await loadClients(dbConnection, companyId);
+    if (companyClients == undefined || Object.keys(clientList).length === 0) {
+      try {
+        await loadClients(dbConnection, companyId);
 
-                companyClients = clientList[companyId];
-            } catch (error) {
-                logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
-                throw companyClients;
-            }
-        }
-
-        return companyClients;
-    } catch (error) {
-        logRed(`Error en getClientsByCompany: ${error.stack}`);
-        throw error;
+        companyClients = clientList[companyId];
+      } catch (error) {
+        logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
+        throw companyClients;
+      }
     }
+
+    return companyClients;
+  } catch (error) {
+    logRed(`Error en getClientsByCompany: ${error.stack}`);
+    throw error;
+  }
 }
 
 async function loadDrivers(dbConnection, companyId) {
-    if (!driverList[companyId]) {
-        driverList[companyId] = {}
-    }
+  if (!driverList[companyId]) {
+    driverList[companyId] = {};
+  }
 
-    try {
-        const queryUsers = `
+  try {
+    const queryUsers = `
             SELECT sistema_usuarios.did, sistema_usuarios.usuario 
             FROM sistema_usuarios_accesos
             INNER JOIN sistema_usuarios ON sistema_usuarios_accesos.did = sistema_usuarios.did
@@ -226,74 +236,78 @@ async function loadDrivers(dbConnection, companyId) {
             AND sistema_usuarios.superado = 0
         `;
 
-        const resultQueryUsers = await executeQuery(dbConnection, queryUsers, []);
+    const resultQueryUsers = await executeQuery(dbConnection, queryUsers, []);
 
-        for (let i = 0; i < resultQueryUsers.length; i++) {
-            const row = resultQueryUsers[i];
+    for (let i = 0; i < resultQueryUsers.length; i++) {
+      const row = resultQueryUsers[i];
 
-            if (!driverList[companyId][row.did]) {
-                driverList[companyId][row.did] = {};
-            }
+      if (!driverList[companyId][row.did]) {
+        driverList[companyId][row.did] = {};
+      }
 
-            driverList[companyId][row.did] = {
-                id: row.id,
-                id_origen: row.id_origen,
-                fecha_sincronizacion: row.fecha_sincronizacion,
-                did: row.did,
-                codigo: row.codigo_empleado,
-                nombre: row.usuario,
-            };
-        }
-    } catch (error) {
-        logRed(`Error en loadDrivers para la compañía ${companyId}: ${error.stack}`);
-        throw error;
+      driverList[companyId][row.did] = {
+        id: row.id,
+        id_origen: row.id_origen,
+        fecha_sincronizacion: row.fecha_sincronizacion,
+        did: row.did,
+        codigo: row.codigo_empleado,
+        nombre: row.usuario,
+      };
     }
+  } catch (error) {
+    logRed(
+      `Error en loadDrivers para la compañía ${companyId}: ${error.stack}`
+    );
+    throw error;
+  }
 }
 
 export async function getDriversByCompany(dbConnection, companyId) {
-    try {
-        let companyDrivers = driverList[companyId];
+  try {
+    let companyDrivers = driverList[companyId];
 
-        if (companyDrivers == undefined || Object.keys(driverList).length === 0) {
-            try {
-                await loadDrivers(dbConnection, companyId);
+    if (companyDrivers == undefined || Object.keys(driverList).length === 0) {
+      try {
+        await loadDrivers(dbConnection, companyId);
 
-                companyDrivers = driverList[companyId];
-            } catch (error) {
-                logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
-                throw companyDrivers;
-            }
-        }
-
-        return companyDrivers;
-    } catch (error) {
-        logRed(`Error en getDriversByCompany para la compañía ${companyId}: ${error.stack}`);
-        throw error;
+        companyDrivers = driverList[companyId];
+      } catch (error) {
+        logRed(`Error al cargar compañías desde Redis: ${error.stack}`);
+        throw companyDrivers;
+      }
     }
+
+    return companyDrivers;
+  } catch (error) {
+    logRed(
+      `Error en getDriversByCompany para la compañía ${companyId}: ${error.stack}`
+    );
+    throw error;
+  }
 }
 
 export async function executeQuery(connection, query, values, log = false) {
-    if (log) {
-        logYellow(`Ejecutando query: ${query} con valores: ${values}`);
-    }
-    try {
-        return new Promise((resolve, reject) => {
-            connection.query(query, values, (err, results) => {
-                if (err) {
-                    if (log) {
-                        logRed(`Error en executeQuery: ${err.message}`);
-                    }
-                    reject(err);
-                } else {
-                    if (log) {
-                        logYellow(`Query ejecutado con éxito: ${JSON.stringify(results)}`);
-                    }
-                    resolve(results);
-                }
-            });
-        });
-    } catch (error) {
-        logRed(`Error en executeQuery: ${error.stack}`);
-        throw error;
-    }
+  if (log) {
+    logYellow(`Ejecutando query: ${query} con valores: ${values}`);
+  }
+  try {
+    return new Promise((resolve, reject) => {
+      connection.query(query, values, (err, results) => {
+        if (err) {
+          if (log) {
+            logRed(`Error en executeQuery: ${err.message}`);
+          }
+          reject(err);
+        } else {
+          if (log) {
+            logYellow(`Query ejecutado con éxito: ${JSON.stringify(results)}`);
+          }
+          resolve(results);
+        }
+      });
+    });
+  } catch (error) {
+    logRed(`Error en executeQuery: ${error.stack}`);
+    throw error;
+  }
 }
