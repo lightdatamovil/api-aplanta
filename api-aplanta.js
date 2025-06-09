48dfb4535a668b2aee5b136ef769f167ee3f121c
Agustt123 <agustintracheskyoficial@gmail.com>
2025-06-09 10:41:25 -0300

    ca

--- api-aplanta.js
+++ api-aplanta.js
@@ -2,7 +2,7 @@ import express, { json, urlencoded } from 'express';
 import a_planta from './route/aplanta.js';
 import { redisClient } from './db.js';
 import { logBlue } from './src/funciones/logsCustom.js';
-import cors from 'cors';    
+import cors from 'cors';
 
 const app = express();
 
@@ -15,23 +15,23 @@ const PORT = process.env.PORT || 13000;
 
 app.use("/api", a_planta)
 app.get('/ping', (req, res) => {
-    const currentDate = new Date();
-    currentDate.setHours(currentDate.getHours()); // Resta 3 horas
-  
-    // Formatear la hora en el formato HH:MM:SS
-    const hours = currentDate.getHours().toString().padStart(2, '0');
-    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
-  
-    const formattedTime = `${hours}:${minutes}:${seconds}`;
-  
-    res.status(200).json({
-      hora: formattedTime
-    });
+  const currentDate = new Date();
+  currentDate.setHours(currentDate.getHours()); // Resta 3 horas
+
+  // Formatear la hora en el formato HH:MM:SS
+  const hours = currentDate.getHours().toString().padStart(2, '0');
+  const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+  const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+
+  const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+  res.status(200).json({
+    hora: formattedTime
   });
+});
 
 await redisClient.connect();
 
 app.listen(PORT, () => {
-    logBlue(`Servidor corriendo en el puerto ${PORT}`);
+  logBlue(`Servidor corriendo en el puerto ${PORT}`);
 });
