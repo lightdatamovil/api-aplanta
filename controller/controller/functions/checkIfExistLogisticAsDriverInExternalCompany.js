8cf853f54960e94d409dae2d9c35d4e9e14e17be
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-08-05 11:44:45 -0300

    Enhance database query execution by adding missing boolean parameter in checkIfExistLogisticAsDriverInExternalCompany and handleExternalFlex functions; update package.json and package-lock.json to include lightdata-tools dependency

--- controller/controller/functions/checkIfExistLogisticAsDriverInExternalCompany.js
+++ controller/controller/functions/checkIfExistLogisticAsDriverInExternalCompany.js
@@ -1,9 +1,11 @@
 
+
 import { executeQuery } from "../../../db.js";
+
 
 export async function checkIfExistLogisticAsDriverInExternalCompany(dbConnection, syncCode) {
     const querySelectSistemUsuariosAccesos = 'SELECT usuario FROM sistema_usuarios_accesos WHERE codvinculacion = ?';
-    const chofer = await executeQuery(dbConnection, querySelectSistemUsuariosAccesos, [syncCode]);
+    const chofer = await executeQuery(dbConnection, querySelectSistemUsuariosAccesos, [syncCode], true);
 
     if (chofer.length == 0) {
         return;
