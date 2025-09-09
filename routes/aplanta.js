c6a8926a70e6c70cdee69efabfe95871a7af8888
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-09-09 12:15:53 -0300

    Refactor: Update lightdata-tools path and version; replace buildHandler with buildHandlerWrapper in routes

--- routes/aplanta.js
+++ routes/aplanta.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { aplanta } from "../controller/aplantaController.js";
-import { buildHandler } from "./_handler.js";
+import { buildHandlerWrapper } from "../src/funciones/build_handler_wrapper.js";
 
 const a_planta = Router();
 
 a_planta.post(
   '/aplanta',
-  buildHandler({
+  buildHandlerWrapper({
     controller: async ({ db, req, company }) => {
       const result = await aplanta(db, req, company);
       return result;
--- routes/clearClient.js
+++ routes/clearClient.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { companiesService } from "../db.js";
-import { buildHandler } from "../routes/_handler.js";
+import { buildHandlerWrapper } from "../src/funciones/build_handler_wrapper.js";
 
 const clients = Router();
 
 clients.post(
     '/clear-client-list',
-    buildHandler({
+    buildHandlerWrapper({
         needsDb: false,
         controller: async () => {
             companiesService.clearClientsCache();
