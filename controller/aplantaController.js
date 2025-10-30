e906197949aa0b8a6ac6c97258c625357449a41e
denu <nissgonzalezm@gmail.com>
2025-10-30 09:56:45 -0300

    a

--- controller/aplantaController.js
+++ controller/aplantaController.js
@@ -130,6 +130,7 @@ export async function aplanta(company, dataQr, userId) {
         return response;
     }
     catch (error) {
+        console.log(dbConfig);
         throw error;
 
     } finally {
