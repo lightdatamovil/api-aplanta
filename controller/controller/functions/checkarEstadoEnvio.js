54cf40ecf7afb675a0d6a08b438c8d22751a5126
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-11-07 08:58:33 -0300

    Update checkearEstadoEnvio function to include companyId parameter for state validation

--- controller/controller/functions/checkarEstadoEnvio.js
+++ controller/controller/functions/checkarEstadoEnvio.js
@@ -1,13 +1,13 @@
 import { executeQuery } from "../../../db.js";
 
 /// Checkea si el envio ya fue puesto a planta, entregado, entregado 2da o cancelado
-export async function checkearEstadoEnvio(dbConnection, shipmentId) {
+export async function checkearEstadoEnvio(dbConnection, shipmentId, companyId) {
     const querySelectEstadoEnvio = 'SELECT estado_envio FROM envios WHERE did = ? and superado = 0 and elim = 0 LIMIT 1 ';
     const estado = await executeQuery(dbConnection, querySelectEstadoEnvio, [shipmentId], true);
 
 
     if (estado.length > 0) {
-        if (estado[0].estado_envio == 5 || estado[0].estado_envio == 9 || estado[0].estado_envio == 8) {
+        if (estado[0].estado_envio == 5 || estado[0].estado_envio == 9 || (companyId != 137 && estado[0].estado_envio == 8)) {
             return { success: false, message: "El paquete ya fue entregado o cancelado" };
         }
         if (estado[0].estado_envio == 1) {
